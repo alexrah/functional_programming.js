af21d8b7c88855f37c3c4add7508271c557b4954
alessandro stoppato <alexrah@gmail.com>
2023-04-02 03:00:19 +0200

    src/curryAndFunctionComposition.ts: add chank colors to console.log outputs

--- src/curryAndFunctionComposition.ts
+++ src/curryAndFunctionComposition.ts
@@ -1,19 +1,21 @@
+import c from "chalk";
+
 const sum = (first:number) => (second:number) => { return first + second }
 
-console.log('sum(3)(5) =',sum(3)(5))
+console.log(c.red('sum(3)(5) ='),sum(3)(5))
 
 const add3 = sum(3);
 const add5 = sum(5);
 
 const pipe = (...fns:Array<(...args:any[])=>any>) => (x: any) => fns.reduce((y,fn) => fn(y),x)
 
-console.log('add3(10) =',add3(10))
+console.log(c.blue('add3(10) ='),add3(10))
 
-console.log('add5(10) =',add5(10));
+console.log(c.blue('add5(10) ='),add5(10));
 
 const pipeAdd3Add5 = pipe(
   add3,
   add5
 );
 
-console.log('pipeAdd3Add5(10)) =', pipeAdd3Add5(10));
+console.log(c.magenta('pipeAdd3Add5(10)) ='), pipeAdd3Add5(10));
